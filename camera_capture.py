c6a92daceabd843394794198a07d5d8cb7d07b18
Mardamüller <mardaguler@gmail.com>
2026-04-20 22:09:02 +0300

    AWS S3 implemented

--- camera_capture.py
+++ camera_capture.py
@@ -3,6 +3,47 @@ import time
 from datetime import datetime
 import cv2
 import numpy as np
+import boto3
+import os
+
+BUCKET_NAME = "rpi-camera-photo-plants"
+REGION = "eu-north-1"
+S3_PREFIX = ""   # boş bırak → root’a atar (frontend direkt görür)
+
+s3 = boto3.client("s3", region_name=REGION)
+
+def upload_to_s3(file_path):
+    try:
+        file_name = os.path.basename(file_path)
+
+        now = datetime.now()
+
+        year = now.strftime("%Y")
+        month = now.strftime("%m")
+        day = now.strftime("%d")
+
+        s3_key = f"{year}/{month}/{day}/{file_name}" # koyulacak path: 2024/06/15/photo.png gibi
+
+        ext = os.path.splitext(file_name)[1].lower()
+        if ext == ".png":
+            content_type = "image/png"
+        elif ext in [".jpg", ".jpeg"]:
+            content_type = "image/jpeg"
+        else:
+            content_type = "application/octet-stream"
+
+        s3.upload_file(
+            file_path,
+            BUCKET_NAME,
+            s3_key,
+            ExtraArgs={"ContentType": content_type}
+        )
+
+        print(f"Uploaded → {s3_key}")
+
+    except Exception as e:
+        print(f"S3 upload error: {e}")
+
 
 data = np.load("Calibration_Results/camera_calibration.npz")
 camera_matrix = data["mtx"]
@@ -24,13 +65,16 @@ frame = camera.capture_array()
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
+
 raw_filename = f"Captured/raw_{timestamp}.png"
 undist_filename = f"Captured/undist_{timestamp}.png"
 # opencv use bgr format, picamera use rgb format, so we need to convert to bgr
 # to work in opencv
 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-# RAW kaydet
+
+#local ve remote kaydet
 cv2.imwrite(raw_filename, frame)
+upload_to_s3(raw_filename)
 
 # =========================
 # UNDISTORT
